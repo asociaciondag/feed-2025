4cd63a26419cbc5652f0959a32e22cb6f4ea2e3b
Oscar Otero <oom@oscarotero.com>
2025-10-26 19:03:20 +0100

    imaxes

--- _config.ts
+++ _config.ts
@@ -1,11 +1,13 @@
 import lume from "lume/mod.ts";
 import top from "top/mod.ts";
+import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume({
   location: new URL("https://dag.gal/feed2025/"),
 });
 
 site.use(top());
+site.use(transformImages());
 site.add("isaac");
 
 export default site;
